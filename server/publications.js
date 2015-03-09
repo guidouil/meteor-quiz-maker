631c401211adaf26205287e42a3a9067ec14eea5
Guidouil <guidouil@gmail.com>
2015-03-09 23:30:01 +0100

    changing host

--- server/publications.js
+++ server/publications.js
@@ -3,7 +3,6 @@ Meteor.publish('Questions', function () {
   var answersCount = Answers.find({owner: this.userId}).count();
 
   if (Roles.userIsInRole(this.userId, "admin") || (questionsCount > 0 && questionsCount === answersCount)) {
-    console.log('yaya');
     return Questions.find({});
   } else {
     return Questions.find({}, {fields: {'answers.correct': 0}});
--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
   // Creating Admin PreProd
-  if (Meteor.users.findOne("QDXmYCJB86Loo3gpG"))
-  Roles.addUsersToRoles("QDXmYCJB86Loo3gpG", ['admin']);
+  if (Meteor.users.findOne("LPu2NR3MH2QnmwP6E"))
+  Roles.addUsersToRoles("LPu2NR3MH2QnmwP6E", ['admin']);
   // Creating Admin Dev
   if (Meteor.users.findOne("o3rjuBkEHmpmfaNhP"))
   Roles.addUsersToRoles("o3rjuBkEHmpmfaNhP", ['admin']);
