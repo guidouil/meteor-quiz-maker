c277844282579db5e63ef36f5bed88dc452f092c
Guidouil <guidouil@gmail.com>
2015-03-17 23:29:38 +0100

    no header for quiz home

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,3 +1,9 @@
 Template.header.helpers({
-  
+  isQuizHome: function () {
+    var currentRoute = Router.current();
+    if (currentRoute.lookupTemplate() === 'Quiz:quizId') {
+      return true;
+    }
+    return false;
+  }
 });
