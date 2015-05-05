b264523c212a3858ea130288cbb9222cc55c7ee8
Guidouil <guidouil@gmail.com>
2015-05-05 10:46:14 +0200

    header fix

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,7 +1,7 @@
 Template.header.helpers({
   isQuizHome: function () {
     var currentRoute = Router.current();
-    if (currentRoute.lookupTemplate() === 'Quiz:quizId') {
+    if (currentRoute.lookupTemplate() === 'Quiz:quizId' || currentRoute.lookupTemplate() === 'Quiz') {
       return true;
     }
     return false;
