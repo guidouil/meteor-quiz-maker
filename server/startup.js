52f9562b127c719f24bbc2106deb67822f37c33b
Guidouil <guidouil@gmail.com>
2015-05-13 00:23:50 +0200

    kadira

--- server/startup.js
+++ server/startup.js
@@ -5,4 +5,5 @@ Meteor.startup(function () {
     var admin = Meteor.users.findOne({});
     Roles.addUsersToRoles(admin._id, ['admin']);
   }
+  Kadira.connect('kTJmnfX58yxDeHpyP', 'cf6c8c53-8765-4863-a1ba-2c631f97d042');
 });
