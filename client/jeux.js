adc4ac89b84c8e05d3ebe80e99e6d07fd2d0595b
Guidouil <guidouil@gmail.com>
2015-09-01 22:02:24 +0200

    fb 2.4

--- client/jeux.js
+++ client/jeux.js
@@ -29,23 +29,21 @@ Meteor.startup(function () {
 });
 
 // Facebook sdk - start
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '416260791889799',
-    xfbml      : true,
-    version    : 'v2.3'
-  });
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '416260791889799',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
 
-  // ADD ADDITIONAL FACEBOOK CODE HERE
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/fr_FR/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
  // Facebook sdk - End
 
 // Google Analytics
